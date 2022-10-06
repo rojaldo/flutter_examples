b0119bca90f3cc9d2079652d7aa35388c016ec25
Daniel <danielrojo@gmail.com>
2022-10-06 13:45:01 +0200

    calculator done 1

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'My Flutter Demo'),
@@ -31,10 +22,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum States {
+  init,
+  firstFigure,
+  secondFigure,
+  result,
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
+  
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -42,11 +40,106 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   String _display = '';
+  String _myDisplay = '';
+  States _state = States.init;
+  num _firstFigure = 0;
+  num _secondFigure = 0;
+  num _result = 0;
+  String _operator = '';
 
-  void onButtonClicked(dynamic value) {
-    print("Button Clicked: $value");
+  num _calculate(){
+    switch(this._operator){
+      case '+':
+        return _firstFigure + _secondFigure;
+      case '-':
+        return _firstFigure - _secondFigure;
+      case '*':
+        return _firstFigure * _secondFigure;
+      case '/':
+        return _firstFigure / _secondFigure;
+      default:
+        return 0;
+    }
+  }
+
+  void _processSymbol(String symbol) {
+    switch(this._state) {
+      case States.init:
+        break;
+      case States.firstFigure:
+        if(symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
+          this._operator = symbol;
+          this._display += symbol;
+          this._state = States.secondFigure;
+        }
+        break;
+      case States.secondFigure:
+        if(symbol == '=') {
+          this._result = this._calculate();
+          this._display += symbol + this._result.toString();
+          this._state = States.result;
+        }
+        break;
+      case States.result:
+        if(symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
+          this._operator = symbol;
+          this._firstFigure = this._result;
+          this._secondFigure = 0;
+          this._result = 0;
+          this._display = this._firstFigure.toString() + symbol;
+          this._state = States.secondFigure;
+        }
+        break;
+      default:
+        throw Exception('Unknown state');
+        break;
+    }
+  }
+
+  void _processNumber(int number) {
+    print(this._state.toString() + ' : ' + States.init.toString());
+    switch(this._state) {
+      case States.init:
+
+        this._display = number.toString();
+        this._firstFigure = number;
+        this._state = States.firstFigure;
+        print('init: ' + this._display);
+        break;
+      case States.firstFigure:
+        this._display += number.toString();
+        this._firstFigure = this._firstFigure * 10 + number;
+        break;
+      case States.secondFigure:
+        this._display += number.toString();
+        this._secondFigure = this._secondFigure * 10 + number;
+        break;
+      case States.result:
+        this._display = number.toString();
+        this._firstFigure = number;
+        this._secondFigure = 0;
+        this._result = 0;
+        this._operator = '';
+        this._state = States.firstFigure;
+        break;
+      default:
+        throw Exception('Unknown state');
+        break;
+    }
+  }
+
+  void _onButtonClicked(dynamic value) {
+    print("Button Clicked: ${value.runtimeType}");
+    if(value.runtimeType == String) {
+      this._processSymbol(value);
+    }else if(value.runtimeType == int) {
+      this._processNumber(value);
+    }else {
+      throw Exception("Invalid type");
+    }
+    print('Display: ' + this._display);
     setState(() {
-      this._display = this._display + value.toString();
+      this._myDisplay = this._display;
     });
   }
 
@@ -76,19 +169,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               child: const Text('7'),
-              onPressed: () => onButtonClicked('7'),
+              onPressed: () => _onButtonClicked(7),
             ),
             ElevatedButton(
               child: const Text('8'),
-              onPressed: () => onButtonClicked('8'),
+              onPressed: () => _onButtonClicked(8),
             ),
             ElevatedButton(
               child: const Text('9'),
-              onPressed: () => onButtonClicked('9'),
+              onPressed: () => _onButtonClicked(9),
             ),
             ElevatedButton(
               child: const Text('/'),
-              onPressed: () => onButtonClicked('/'),
+              onPressed: () => _onButtonClicked('/'),
             ),
           ],
         ),
@@ -97,19 +190,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               child: const Text('4'),
-              onPressed: () => onButtonClicked('4'),
+              onPressed: () => _onButtonClicked(4),
             ),
             ElevatedButton(
               child: const Text('5'),
-              onPressed: () => onButtonClicked('5'),
+              onPressed: () => _onButtonClicked(5),
             ),
             ElevatedButton(
               child: const Text('6'),
-              onPressed: () => onButtonClicked('6'),
+              onPressed: () => _onButtonClicked(6),
             ),
             ElevatedButton(
               child: const Text('*'),
-              onPressed: () => onButtonClicked('*'),
+              onPressed: () => _onButtonClicked('*'),
             ),
           ],
         ),
@@ -118,19 +211,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               child: const Text('1'),
-              onPressed: () => onButtonClicked('1'),
+              onPressed: () => _onButtonClicked(1),
             ),
             ElevatedButton(
               child: const Text('2'),
-              onPressed: () => onButtonClicked('2'),
+              onPressed: () => _onButtonClicked(2),
             ),
             ElevatedButton(
               child: const Text('3'),
-              onPressed: () => onButtonClicked('3'),
+              onPressed: () => _onButtonClicked(3),
             ),
             ElevatedButton(
               child: const Text('-'),
-              onPressed: () => onButtonClicked('-'),
+              onPressed: () => _onButtonClicked('-'),
             ),
           ],
         ),
@@ -139,19 +232,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               child: const Text('c'),
-              onPressed: () => onButtonClicked('c'),
+              onPressed: () => _onButtonClicked('c'),
             ),
             ElevatedButton(
               child: const Text('0'),
-              onPressed: () => onButtonClicked('0'),
+              onPressed: () => _onButtonClicked(0),
             ),
             ElevatedButton(
               child: const Text('='),
-              onPressed: () => onButtonClicked('='),
+              onPressed: () => _onButtonClicked('='),
             ),
             ElevatedButton(
               child: const Text('+'),
-              onPressed: () => onButtonClicked('+'),
+              onPressed: () => _onButtonClicked('+'),
             ), // ElevatedButton
           ], // <Widget>[] children of Row
         ), // Row
