c56b718dcd8b3835f20d93fb85109611504a2338
Daniel <danielrojo@gmail.com>
2022-10-11 16:44:26 +0200

    floating button added

--- lib/pages/apod/apodpage.dart
+++ lib/pages/apod/apodpage.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample/pages/apod/apodwidget.dart';
 
-class Apodpage extends StatelessWidget {
+import '../../models/apod.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Apodpage extends StatefulWidget {
   const Apodpage({Key? key}) : super(key: key);
+
+  @override
+  _ApodpageState createState() => _ApodpageState();
+}
+
+class _ApodpageState extends State<Apodpage> {
+  String _code = '';
+  Apod _apod = Apod();
+  dynamic _selectedDate = DateTime.now();
+
+  Future<http.Response> _fetchData(String url) {
+    return http.get(Uri.parse(url));
+  }
+
+  void _getApod([dateString = '']) {
+    String key = 'DEMO_KEY';
+    String url =
+        'https://api.nasa.gov/planetary/apod?api_key=$key&date=$dateString';
+    print(url);
+    _fetchData(url).then((response) {
+      setState(() {
+        _code = response.statusCode.toString();
+        _apod = Apod.fromJson(jsonDecode(response.body));
+      });
+    });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(1995, 6, 15),
+        lastDate: DateTime.now());
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+        _getApod(pickedDate.toString().substring(0, 10));
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getApod();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +60,9 @@ class Apodpage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Apod Sample'),
         ),
-        body: const ApodWidget(),
+        body: ApodWidget(myApod: _apod),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => _selectDate(context),
           tooltip: 'Increment',
           child: const Icon(Icons.calendar_month),
         )); // Scaffold
--- lib/pages/apod/apodwidget.dart
+++ lib/pages/apod/apodwidget.dart
@@ -1,57 +1,12 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import '../../models/apod.dart';
 import '../../models/apod.dart';
 
-class ApodWidget extends StatefulWidget {
-  const ApodWidget({Key? key}) : super(key: key);
-
-  @override
-  _ApodWidgetState createState() => _ApodWidgetState();
-}
-
-class _ApodWidgetState extends State<ApodWidget> {
-  String _code = '';
+class ApodWidget extends StatelessWidget {
   Apod _apod = Apod();
-  dynamic _selectedDate = DateTime.now();
-
-  Future<http.Response> _fetchData(String url) {
-    return http.get(Uri.parse(url));
+  ApodWidget({Key? key, required Apod myApod}) : super(key: key) {
+    _apod = myApod;
   }
-
-  void _getApod([dateString = '']) {
-    String key = 'DEMO_KEY';
-    String url =
-        'https://api.nasa.gov/planetary/apod?api_key=$key&date=$dateString';
-    _fetchData(url).then((response) {
-      setState(() {
-        _code = response.statusCode.toString();
-        _apod = Apod.fromJson(jsonDecode(response.body));
-      });
-    });
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-        context: context,
-        initialDate: _selectedDate,
-        firstDate: DateTime(1995, 6, 15),
-        lastDate: DateTime.now());
-    if (pickedDate != null) {
-      setState(() {
-        _selectedDate = pickedDate;
-        _getApod(pickedDate.toString().substring(0, 10));
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getApod();
-  }
-
   Widget _showMedia() {
     if (_apod.isImage) {
       return Image.network(_apod.url);
@@ -70,10 +25,6 @@ class _ApodWidgetState extends State<ApodWidget> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () => _selectDate(context),
-              child: const Text('Select date'),
-            ),
             Card(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
